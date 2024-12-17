6b85dfd87ed6c8e3bb52d477d1805911115b963c
RonnieSamson <166392656+RonnieSamson@users.noreply.github.com>
2024-12-17 11:23:22 +0100

    added properties do user

--- Concert.Data/Entity/User.cs
+++ Concert.Data/Entity/User.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Concert.Data.Entity
 {
     internal class User
     {
+        [Key]
+        [StringLength(36)]
+        [MinLength(10)]
+        public required string ID { get; set; }
+        [StringLength(30)]
+        public required string name { get; set; }
+        [StringLength(50)]
+        public required string Email { get; set; }
+        [StringLength(30)]
+        [MinLength(6)]
+        public required string Password { get; set; }
+
+        public List<Booking> Bookings { get; set; }
     }
 }
