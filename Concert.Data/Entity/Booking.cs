eb884010a92d0f3edf1172b08a06b07c41358766
Christian Vittaniemi <crv87@outlook.com>
2024-12-17 11:22:53 +0100

    Booking och Consert

--- Concert.Data/Entity/Booking.cs
+++ Concert.Data/Entity/Booking.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Concert.Data.Entity
 {
-    internal class Booking
+    public class Booking
     {
+        [Key]
+        
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Email { get; set; }
+
+        public List<User>  { get; set; }
     }
 }
