5e8311b2650757c33950ada013baf223fcc72f43
RonnieSamson <166392656+RonnieSamson@users.noreply.github.com>
2025-01-28 17:02:04 +0100

    added UserDto classs

---
+++ Consert.Data.DTO/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consert.Data.DTO
+{
+    internal class UserDto
+    {
+        public string ID { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
