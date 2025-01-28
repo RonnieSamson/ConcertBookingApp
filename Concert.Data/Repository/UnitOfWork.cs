6d2435bcfd2ebebcfdef87bd97f43954d3c177d8
RonnieSamson <166392656+RonnieSamson@users.noreply.github.com>
2025-01-28 16:37:05 +0100

    added code to UnitOfWork

--- Concert.Data/Repository/UnitOfWork.cs
+++ Concert.Data/Repository/UnitOfWork.cs
@@ -8,6 +8,24 @@ using Concert.Data.Entity;
 
 namespace Concert.Data.Repository
 {
-    
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext context;
+        public IUserRepository Users { get; private set; }
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            this.context = context;
+            Users = new UserRepository(context);
+        }
+        public async Task<int> CompleteAsync()
+        {
+            return await context.SaveChangesAsync();
+        }
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+
+    }
 
 }
