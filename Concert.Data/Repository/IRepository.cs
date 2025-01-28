fd9dcc367968a839dc6852655eece3c0f25bc3c6
RonnieSamson <166392656+RonnieSamson@users.noreply.github.com>
2025-01-28 14:58:07 +0100

    Created Interfaces, repositories,  unitofwork classses

---
+++ Concert.Data/Repository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Concert.Data.Entity;
+using System.Linq.Expressions;
+
+namespace Concert.Data.Repository
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> All();
+        Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
+        void Insert(TEntity entity);
+        void InsertRange(IEnumerable<TEntity> entities);
+        void Delete(TEntity entity);
+        void DeleteRange(IEnumerable<TEntity> entities);
+    }
+}
