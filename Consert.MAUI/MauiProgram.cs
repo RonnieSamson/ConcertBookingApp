bbde8a42c276abfbf70cd163a3377f79d54d723b
RonnieSamson <166392656+RonnieSamson@users.noreply.github.com>
2025-02-13 21:30:57 +0100

    uppdated maui commmunity toolkit

--- Consert.MAUI/MauiProgram.cs
+++ Consert.MAUI/MauiProgram.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.Logging;
 
 namespace Consert.MAUI
 {
+   
+
     public static class MauiProgram
     {
         public static MauiApp CreateMauiApp()
@@ -10,13 +12,16 @@ namespace Consert.MAUI
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
-                .UseMauiCommunityToolkit();
+                // Initialize the .NET MAUI Community Toolkit by adding the below line of code
+                .UseMauiCommunityToolkit()
+                // After initializing the .NET MAUI Community Toolkit, optionally add additional fonts
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
 
-
-
-#if DEBUG
-            builder.Logging.AddDebug();
-#endif
+            // Continue initializing your .NET MAUI App here
 
             return builder.Build();
         }
