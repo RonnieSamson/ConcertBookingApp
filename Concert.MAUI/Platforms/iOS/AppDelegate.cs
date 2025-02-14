821b043eaa00426f7a9a3aef558e5b95473d00b7
RonnieSamson <166392656+RonnieSamson@users.noreply.github.com>
2025-02-14 14:30:56 +0100

    added maui

---
+++ Concert.MAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Concert.MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ Consert.MAUI/Profiles/BookingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Consert.MAUI.Models;
+using Concert.Data.DTO;
+
+namespace Consert.MAUI.Profiles
+{
+    public class BookingProfile : Profile
+    {
+        public BookingProfile()
+        {
+            
+        }
+    }
+}
