3bf99a17e1a0e958abac359f71e222457a75327e
RonnieSamson <166392656+RonnieSamson@users.noreply.github.com>
2025-02-22 14:52:19 +0100

    modyfied HomePageViewModel

--- Concert.MAUI/ViewModels/HomePageViewModel.cs
+++ Concert.MAUI/ViewModels/HomePageViewModel.cs
@@ -11,14 +11,21 @@ namespace Concert.MAUI.ViewModels
     public partial class HomepageViewModel : ObservableObject
     {
         private readonly IConcertService _concertService;
+        private readonly IPerformanceService _performanceService;
+        private readonly IBookingService _bookingService;
 
         [ObservableProperty]
         private ObservableCollection<Concert.MAUI.Models.Concert> concerts = new();
 
-        public HomepageViewModel(IConcertService concertService)
+        [ObservableProperty]
+        private Dictionary<string, ObservableCollection<Performance>> concertPerformances = new();
+
+        public HomepageViewModel(IConcertService concertService, IPerformanceService performanceService, IBookingService bookingService)
         {
             _concertService = concertService;
-            
+            _performanceService = performanceService;
+            _bookingService = bookingService;
+
         }
 
         [RelayCommand]
@@ -28,10 +35,37 @@ namespace Concert.MAUI.ViewModels
             if (concertsList != null)
             {
                 Concerts.Clear();
+                concertPerformances.Clear();
                 foreach (var concert in concertsList)
                 {
                     Concerts.Add(concert);
+                    concertPerformances[concert.ConcertId] = new ObservableCollection<Performance>();
                 }
+            }
+        }
+        [RelayCommand]
+        private async Task LoadPerformances(string concertId)
+        {
+            if (concertPerformances.ContainsKey(concertId) && concertPerformances[concertId].Count == 0)
+            {
+                var performances = await _performanceService.GetPerformancesByConcertIdAsync(concertId);
+                if (performances != null)
+                {
+                    foreach (var performance in performances)
+                    {
+                        concertPerformances[concertId].Add(performance);
+                    }
+                }
+            }
+        }
+
+        [RelayCommand]
+        private async Task BookPerformance(string concertId, string userId)
+        {
+            bool sucess = await _bookingService.BookPerformanceAsync(userId, concertId);
+            if (sucess)
+            {
+                
             }
         }
     }
