86565bb271a11a89f1759edc4e59ce2d936da56a
RonnieSamson <166392656+RonnieSamson@users.noreply.github.com>
2025-02-24 21:00:51 +0100

    updated DTO en performanceDetailpage

--- Consert.API/Profiles/UserProfile.cs
+++ Consert.API/Profiles/UserProfile.cs
@@ -13,8 +13,8 @@ namespace Consert.API.Profiles
             CreateMap<User, UserDto>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
-                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
-                .ForMember(dest => dest.Password, opt => opt.MapFrom(src => src.Password));
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email));
+                //.ForMember(dest => dest.Password, opt => opt.MapFrom(src => src.Password));
 
             CreateMap<UserDto, User>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
